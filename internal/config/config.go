1d062b54d19c56243546e890aafb84bdb739eaa5
alo-thet <thetnaungsoe@alo.co.uk>
2026-06-23 14:24:35 +0100

    finish Config

---
+++ internal/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppEnv  string
+	AppPort string
+
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBSSLMode  string
+}
+
+func Load() *Config {
+	err := godotenv.Load()
+
+	if err != nil {
+		return nil
+	}
+
+	return &Config{
+		AppEnv:  getEnv("APP_ENV", "development"),
+		AppPort: getEnv("APP_PORT", "8080"),
+
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "password"),
+		DBName:     getEnv("DB_NAME", "go_starter"),
+		DBSSLMode:  getEnv("DB_SSLMODE", "disable"),
+	}
+
+}
+
+func getEnv(key string, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+
+	return value
+}
