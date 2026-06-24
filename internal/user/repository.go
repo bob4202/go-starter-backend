4f2b06c62185b6a6e1063aad0f6c4ba623f7c196
alo-thet <thetnaungsoe@alo.co.uk>
2026-06-24 10:09:28 +0100

    Finish the user repo related thing

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -29,3 +29,30 @@ func (r *Repository) FindByEmail(email string) (*User, error) {
 	return &user, nil
 
 }
+
+func (r *Repository) CreateUser(u *User) error {
+
+	query := `
+	INSERT INTO users(name, email, password_hash) 
+	VALUES ($1, $2, $3)
+	RETURNING
+		id, created_at, updated_at, is_active
+	`
+
+	return r.db.QueryRow(query, u.Name, u.Email, u.PasswordHash).Scan(&u.ID, &u.CreatedAt, &u.UpdatedAt, &u.IsActive)
+
+}
+
+func (r *Repository) UpdateUser(u *User) error {
+	query := `
+	UPDATE users 
+	SET name=$2,email=$3, password_hash=$4, is_active=$5,updated_at=NOW(),deleted_at=$7
+	WHERE
+		id = $1
+	RETURNING
+		name,email, is_active,updated_at,deleted_at
+	`
+
+	return r.db.QueryRow(query, u.Name, u.Email, u.PasswordHash, u.IsActive, u.UpdatedAt, u.DeletedAt).Scan(&u.Name, &u.Email, &u.IsActive, &u.UpdatedAt, u.DeletedAt)
+
+}
